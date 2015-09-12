37eb6901c13c1363056ca898c718f667f62b89b2
vinay427 <vhiremath95@gmail.com>
2015-09-12 17:16:48 -0400

    Fixed API with auth and song endpoints

--- app.js
+++ app.js
@@ -1,60 +1,101 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var SpotifyWebApi = require('spotify-web-api-node');
+var Promise = require('promise');
 
 var code = process.env.SPOTIFY_ACCESS_CODE;
-var access_token = process.env.SPOTIFY_ACCESS_TOKEN;
-var refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
 var app = express();
+app.use(bodyParser.json());
 var spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    accessToken: access_token,
-    refreshToken: refresh_token,
     redirectUri: 'https://localhost/callback'
 });
 
 console.log('Client ID: ' + process.env.SPOTIFY_CLIENT_ID);
 console.log('Client Secret: ' + process.env.SPOTIFY_CLIENT_SECRET);
 
-/*
-spotifyApi.authorizationCodeGrant(code)
-    .then(function(data) {
-
-        console.log('The access token is ' + data.body['access_token']);
-        console.log('The refresh token is ' + data.body['refresh_token']);
-        spotifyApi.setAccessToken(data.body['access_token']);
-        spotifyApi.setRefreshToken(data.body['refresh_token']);
-    }, function(err) {
-        console.log('Something went wrong!', err); // Cannot get authorization
+app.get('/auth', function(req, res) {
+    res.json({
+        url: spotifyApi.createAuthorizeURL([], 'state')
     });
- */
+});
 
-spotifyApi.refreshAccessToken()
-    .then(function(data) {
-        console.log('The access token has been refreshed!');
-        console.log('The token expires in: ' + data.body['expires_in']);
-    }, function(err) {
-        console.log('Could not refresh access token', err);
-    });
-
-app.get('/', function(req, res) {
-    spotifyApi.searchPlaylists('workout')
+app.post('/auth', function(req, res) {
+    console.log(req.body);
+    spotifyApi.authorizationCodeGrant(req.body.code)
         .then(function(data) {
-            var playlists = data.body.playlists.items;
-            playlists.map(function(playlist) {
-                console.log('User: ' + playlist.owner.id);
-                console.log('ID: ' + playlist.id);
-
-                // Get tracks in playlist
-                spotifyApi.getPlaylistTracks(playlist.owner.id, playlist.id)
-                    .then(function(tracks) { console.log(tracks); },
-                          function(err) { console.log('Error: ' + err); }); // Cannot get tracks
+            console.log('The access token is ' + data.body['access_token']);
+            console.log('The refresh token is ' + data.body['refresh_token']);
+            res.json({
+                access: data.body['access_token'],
+                refresh: data.body['refresh_token']
             });
         }, function(err) {
             res.status(500);
-            console.log('Something went wrong!', err); // Cannot get playlists
+            console.log('Something went wrong!', err); // Cannot get authorization
         });
+});
+
+function getSongByMood(mood) {
+    return spotifyApi.searchPlaylists(mood)
+        .then(function(res) {
+            var allPlaylists = res.body.playlists.items;
+            var playlist = allPlaylists[Math.floor(Math.random() * allPlaylists.length)]; // Choose a random playlist
+            console.log('Mood: %s \t Playlist User: %s \t Playlist ID: %s', mood, playlist.owner.id, playlist.id);
+            return spotifyApi.getPlaylistTracks(playlist.owner.id, playlist.id)
+                .then(function(res) {
+                    var allTracks = res.body.items;
+                    var track = allTracks[Math.floor(Math.random() * allTracks.length)]; // Choose a random track
+                    console.log('Mood: %s \t Track: %s', mood, track.track.preview_url);
+                    return track.track.preview_url;
+                }, function(err) {
+                    console.log('Track for playlist for mood %s cannot be processed.\n Error: %s', mood, err);
+                    return false;
+                });
+        }, function(err) {
+            console.log('Playlist for mood %s cannot be processed.\n Error: %s', mood, err);
+            return false;
+        });
+}
+
+app.get('/song', function(req, res) {
+    // SET HEADER AUTH STUFF
+    spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
+    spotifyApi.setRefreshToken(process.env.SPOTIFY_REFRESH_TOKEN);
+    Promise.all([
+        getSongByMood('neutral'),
+        getSongByMood('excited'),
+        getSongByMood('relaxed')
+    ]).then(function (tracks) {
+        console.log(tracks[0], tracks[1], tracks[2]);
+        var neutralTrack = tracks[0];
+        var excitedTrack = tracks[1];
+        var relaxedTrack = tracks[2];
+
+        if (!neutralTrack || !excitedTrack || !relaxedTrack) {
+            console.log('Error');
+            res.status(500);
+            res.json({
+                status: 'error',
+                body: 'Error with getting tracks.'
+            });
+        } else {
+
+            console.log('Success');
+            res.json({
+                status: 'success',
+                body: {
+                    neutral: neutralTrack,
+                    excited: excitedTrack,
+                    relaxed: relaxedTrack
+                }
+            });
+        }
+        res.end();
+    }).catch(console.error.bind(console));
+
 });
 
 /*
